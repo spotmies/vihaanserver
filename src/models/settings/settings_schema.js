79c53d0f5d299df1be5e1ac83f5aa0c344d544ad
modernsilpi <modernsilpi@gmail.com>
2021-11-17 16:31:48 +0530

    settings schema2

--- src/models/settings/settings_schema.js
+++ src/models/settings/settings_schema.js
@@ -9,9 +9,40 @@ const {
   createdAt,
 } = require("../../helpers/schemaHelp");
 
+const settingsSchema = new Mongoose.Schema({
+  login: {
+    head: defaultString,
+    caption: defaultString,
+    uname: defaultString,
+    forgotPwd: defaultString,
+    loginButton: defaultString,
+    password: defaultString,
+    noAccount: defaultString,
+  },
 
-const settingsSchema = new Mongoose.Schema(
+  welcome: {
+    head: defaultString,
+    caption: defaultString,
+    nextButton: defaultString,
+  },
 
-);
+  onBoard: [
+    {
+      title: defaultString,
+      content: defaultString,
+      img: defaultString,
+      button1: {
+        text: defaultString,
+        textColor: defaultString,
+        color: defaultString,
+      },
+      button2: {
+        text: defaultString,
+        color: defaultString,
+        textColor: defaultString,
+      },
+    },
+  ],
+});
 
 module.exports = Mongoose.model("settings", settingsSchema);
