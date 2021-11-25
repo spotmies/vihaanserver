1c86bf40ee5f4d471f64742049261635646f66bf
modernsilpi <modernsilpi@gmail.com>
2021-11-25 11:32:45 +0530

    api urls changed

--- src/routes/main_router.js
+++ src/routes/main_router.js
@@ -5,11 +5,13 @@ const router = express.Router();
 const userR = require("./users/user");
 const settingsR = require("./settings/settings");
 const productsR = require("./products/products");
+const testDriveR = require("./test_drive/test_drive");
 
 // import userR from "./users/user";
 
 router.use("/user", userR);
 router.use("/settings", settingsR);
 router.use("/product", productsR);
+router.use("/test-ride", testDriveR);
 
 module.exports = router;
