7837cc0fa1d5feef2ee140a9b94ac3d857879c4f
modernsilpi <modernsilpi@gmail.com>
2021-12-20 16:23:03 +0530

    working on 503 error

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ var cors = require("cors");
 const mainRoute = require("./src/routes/main_router");
 
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 const PORT = process.env.PORT || 4000;
 const server = require("http").createServer(app);
 const connectdb = require("./src/config/db");
 
 connectdb();
 app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+//   next();
+// });
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
