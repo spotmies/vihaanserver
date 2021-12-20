ca1085f0c9709bd183daae2ccde4e5b735b6b6d0
modernsilpi <modernsilpi@gmail.com>
2021-12-20 16:13:40 +0530

    added cors for headers

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 var cors = require("cors");
 const mainRoute = require("./src/routes/main_router");
 
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 4000;
-const server = require('http').createServer(app);
-const connectdb = require('./src/config/db');
+const server = require("http").createServer(app);
+const connectdb = require("./src/config/db");
 
 connectdb();
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -19,5 +26,5 @@ app.use(bodyParser.json());
 app.use("/api", mainRoute);
 
 server.listen(PORT, () => {
-    console.log(`app running on port ${PORT}`);
-  });
+  console.log(`app running on port ${PORT}`);
+});
